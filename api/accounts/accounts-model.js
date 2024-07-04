d7f5af249e2e9e01823cffe2cce2a2c413409dcd
Robert Shepherd <robertsh26@outlook.com>
2024-07-04 13:16:34 -0700

    main

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,23 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  return db('accounts');
 }
 
 const getById = id => {
-  // DO YOUR MAGIC
+  return db('accounts').where('id', id).first()
 }
 
 const create = account => {
-  // DO YOUR MAGIC
+  
 }
 
 const updateById = (id, account) => {
-  // DO YOUR MAGIC
+  
 }
 
 const deleteById = id => {
-  // DO YOUR MAGIC
+  
 }
 
 module.exports = {
