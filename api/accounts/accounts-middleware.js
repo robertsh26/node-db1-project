0a4e2f54fa0905cd9530808851674fc3418f4739
Robert Shepherd <robertsh26@outlook.com>
2024-07-04 13:23:09 -0700

    main

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,3 +1,5 @@
+const Account = require('./accounts-model')
+
 exports.checkAccountPayload = (req, res, next) => {
   console.log('checkAccountPayload middleware')
   next()
@@ -8,7 +10,16 @@ exports.checkAccountNameUnique = (req, res, next) => {
   next()
 }
 
-exports.checkAccountId = (req, res, next) => {
-  console.log('checkAccountNameUnique middleware')
-  next()
+exports.checkAccountId = async (req, res, next) => {
+  try {
+    const account = await Account.getById(req.params.id)
+    if (!account) {
+      next({ status: 404, message: 'not found'})
+    } else {
+      req.account = account
+      next()
+    }
+  } catch(err) {
+    next(err)
+  }
 }
